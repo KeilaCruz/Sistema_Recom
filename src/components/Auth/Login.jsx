855464104b11942584a42315a477a422d1b8b7e1
Juan Carlos LV <lopesjuancarlos216@gmail.com>
2023-11-22 04:51:05 -0600

    Add styles in Login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../partials/headers/HeaderLogin'
+import {Link} from 'react-router-dom'
 
 function Login() {
   return (
     <main className='flex flex-col overflow-hidden font-sans'>
         <Header/>
 
-        <section className='m-[40px] shadow flex flex-row p-[35px] h-[500px]  items-center gap-[220px] '>
+        <section className='m-[40px] shadow flex flex-row p-[20px] items-center gap-[220px] '>
 
           <article className='flex flex-col gap-[40px] items-center'>
             <h1 className='text-[#3B315F] text-[40px] font-bold '>RYCO <br /> Taller de Torno y Soldadura</h1>
@@ -15,9 +16,20 @@ function Login() {
             </picture>
           </article>
 
-          <section className='flex-col'>
-            <p className='text-[40px] text-[#3B315F] font-bold'>Iniciar Sesión</p>
+          <section className='flex flex-col'>
+            <p className='text-[40px] text-[#3B315F] font-bold mb-[40px]'>Iniciar Sesión</p>
+            <section className='flex flex-col'>
+              <form action="" className='flex flex-col gap-3 font-sans '>
+                <label htmlFor="" className='text-[25px] font-semibold'>Usuario</label>
+                <input type="text" placeholder='Ingresa tu usuario' className='border-[1px] border-[#3B315F] p-[10px] rounded-[5px] w-[450px] text-[22px] visited:border-black '/>
+                <label htmlFor="" className='text-[25px] font-semibold'>Contraseña</label>
+                <input type="password" placeholder='Ingresa tu contraseña' className='border-[1px] border-[#3B315F] p-[10px] rounded-[5px] w-[450px] text-[22px]'/>
+              </form>
 
+              <Link to="/reestablecer-contraseña" className=' text-[20px] text-[#3B315F] underline font-semibold mt-[20px] hover:text-[#4D407E]'>¿Olvidaste tu contraseña?</Link> 
+              <button className='mt-[50px] bg-[#3B315F] p-[10px] text-white text-[20px] font-sans font-medium rounded hover:bg-[#4D407E]'>Ingresar</button>  
+      
+            </section>
           </section>
             
         </section>
