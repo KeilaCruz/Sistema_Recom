b54be65445d5726c723726e62b086522e431ebd4
Juan Carlos LV <lopesjuancarlos216@gmail.com>
2023-11-27 16:29:44 -0600

    Mergin branch Juan y May

--- src/Routes/Rutas.jsx
+++ src/Routes/Rutas.jsx
@@ -5,6 +5,14 @@ import HomeAdministrador from "../Views/V_HomeAdministrador.jsx";
 import RecuperarContraseña from "../Views/V_RecuperarContraseña";
 import NuevaContraseña from "../components/Auth/NuevaContraseña.jsx";
 import NotFound from "../Views/NotFound";
+import { RegistrarOrden } from "./components/Administrador/RegistrarOrden";
+import { RegistrarTrabajador } from "./components/Administrador/RegistrarTrabajador";
+import { VisualizarOrdenes } from "./components/Administrador/VisualizarOrdenes";
+import { Toaster } from "react-hot-toast";
+import { VisualizarTrabajadores } from "./components/Administrador/VisualizarTrabajadores";
+import { VisualizarTrabajador } from "./components/Administrador/VisualizarTrabajador";
+
+import { Toaster } from 'react-hot-toast'
 
 function Rutas() {
   return (
@@ -18,11 +26,22 @@ function Rutas() {
         element={<RecuperarContraseña />}
       ></Route>
 
+      <Route path="/nueva-contraseña" element={<NuevaContraseña />}></Route>
+
+      <Route path="/registrar-orden-trabajo" element={<RegistrarOrden />} />
+      <Route path="/registrar-trabajador" element={<RegistrarTrabajador />} />
+      <Route path="/visualizar-ordenes" element={<VisualizarOrdenes />} />
       <Route
-        path="/nueva-contraseña"
-        element={<NuevaContraseña />}
-      ></Route>
+        path="/visualizar-trabajadores"
+        element={<VisualizarTrabajadores />}
+      />
+      <Route
+        path="/visualizar-trabajador/:id"
+        element={<VisualizarTrabajador />}
+      />
+      <Toaster/>
     </Routes>
+    
   );
 }
 
