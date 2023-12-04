f9ca81f5887478adc8fc06c20e6b89029ee63030
Mayreli Romero <may70981@gmail.com>
2023-12-04 12:27:11 -0600

    Agregué un poco de espaciado, aun falta

--- src/components/Administrador/RegistrarOrden.jsx
+++ src/components/Administrador/RegistrarOrden.jsx
@@ -126,66 +126,117 @@ export function RegistrarOrden() {
 
     return (
         <>
-            <form onSubmit={onSubmit}>
-                <label> Fecha solicitud
-                    <input id="fecha_solicitud" type="date" readOnly {...register("fecha_solicitud", { required: true })} />
+            <form className="flex flex-col gap-4 font-sans"
+                onSubmit={onSubmit}>
+
+                <div className="flex ml-10">
+                    <label className="text-[20px] font-semibold"> Fecha solicitud </label>
+                    <input id="fecha_solicitud" type="date"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F] ml-4"
+                        readOnly {...register("fecha_solicitud", { required: true })} />
                     {errors.fecha_solicitud && <span>Este campo es necesario</span>}
-                </label>
-                <label> Fecha entrega
-                    <input id="fecha_entrega" type="date" {...register("fecha_entrega", { required: true })} />
+
+                    <label className="text-[20px] font-semibold ml-10"> Fecha entrega </label>
+                    <input id="fecha_entrega" type="date"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F] ml-4"
+                        {...register("fecha_entrega", { required: true })} />
                     {errors.fecha_entrega && <span>Este campo es necesario</span>}
-                </label>
-                <label> Precio del trabajo
-                    <input id="precio_trabajo" type="number" step="any" {...register("precio", { required: true })} />
+                </div>
+
+                <div className="flex ml-10" >
+                    <label className="text-[20px] font-semibold"> Precio del trabajo </label>
+                    <input id="precio_trabajo" type="number" step="any"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[100px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        {...register("precio", { required: true })} />
                     {errors.precio && <span>Este campo es necesario</span>}
-                </label>
-                <label> Especificaciones del trabajo
-                    <textarea id="descripcion" placeholder="Escriba los detalles del trabajo a realizar" {...register("especificaciones", { required: true })}></textarea>
+
+                    <label className="text-[20px] font-semibold"> Especificaciones del trabajo </label>
+                    <textarea id="descripcion"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Escriba los detalles del trabajo a realizar" {...register("especificaciones", { required: true })}></textarea>
                     {errors.especificaciones && <span>Este campo es necesario</span>}
-                </label>
-                <label> Nombre
-                    <input id="nom_cliente" type="text" placeholder="Ej. Eduardo" {...register("nombre", { required: true })} />
+                </div>
+
+                <div className="flex ml-10">
+                    <label className="text-[20px] font-semibold"> Nombre </label>
+                    <input id="nom_cliente" type="text"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Ej. Eduardo" {...register("nombre", { required: true })} />
                     {errors.nombre && <span>Este campo es necesario</span>}
-                </label>
-                <label> Apellido paterno
-                    <input id="ape_paterno" type="text" placeholder="Ej. Pérez" {...register("ape_paterno", { required: true })} />
+
+                    <label className="text-[20px] font-semibold"> Apellido paterno </label>
+                    <input id="ape_paterno" type="text"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Ej. Pérez" {...register("ape_paterno", { required: true })} />
                     {errors.ape_paterno && <span>Este campo es necesario</span>}
-                </label>
-                <label> Apellido materno
-                    <input id="ape_materno" type="text" placeholder="Ej. Ruiz" {...register("ape_materno", { required: true })} />
+
+                    <label className="text-[20px] font-semibold"> Apellido materno </label>
+                    <input id="ape_materno" type="text"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Ej. Ruiz" {...register("ape_materno", { required: true })} />
                     {errors.ape_materno && <span>Este campo es necesario</span>}
-                </label>
-                <label> Correo
-                    <input id="correo" type="email" placeholder="Ej. edu12@" {...register("correo", { required: true })} />
+                </div>
+
+                <div className="flex ml-10">
+                    <label className="text-[20px] font-semibold"> Correo </label>
+                    <input id="correo" type="email"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Ej. edu12@" {...register("correo", { required: true })} />
                     {errors.correo && <span>Este campo es necesario</span>}
-                </label>
-                <label> Teléfono
-                    <input id="telefono" type="text" placeholder="Ej. 9212834738" {...register("telefono", { required: true })} />
+
+                    <label className="text-[20px] font-semibold"> Teléfono </label>
+                    <input id="telefono" type="text"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Ej. 9212834738" {...register("telefono", { required: true })} />
                     {errors.telefono && <span>Este campo es necesario</span>}
-                </label>
-                <select id="tipo_trabajo" {...register("tipotrabajo", { required: true })}>
-                    <option value="" disabled>Elija una opción</option>
-                    <option value="nuevo">Nuevo</option>
-                    <option value="reparacion">Reparación</option>
-                </select>
-                {errors.tipotrabajo && <span>Este campo es necesario</span>}
-                <label>
-                    <textarea id="materiales_requeridos" placeholder="Materiales requeridos" {...register("materialtrabajo")}></textarea>
-                </label>
-                <label> Precio del material
-                    <input id="precio_material" type="number" placeholder="Precio del material" step="any" {...register("preciomaterial")} />
-                </label>
-                <label> Seleccione el trabajador
-                    {trabajadores.map((trabajador) => (
-                        <div key={trabajador.idtrabajador}>
-                            <input id="trabaador" type="checkbox" value={trabajador.idtrabajador} onChange={() => handleCheckboxChange(trabajador.idtrabajador)} />
-                            <label htmlFor={`trabajador_${trabajador.idtrabajador}`}>
-                                {trabajador.nom_trabajador} {trabajador.apepaterno} {trabajador.apematerno} {trabajador.tipotrabajador}
-                            </label>
-                        </div>
-                    ))}
-                </label>
-                <button>Registrar</button>
+
+                </div>
+
+                <div className="flex ml-10">
+                    <label className="text-[20px] font-semibold"> Tipo de trabajo </label>
+                    <select id="tipo_trabajo"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        {...register("tipotrabajo", { required: true })}>
+                        <option value="" disabled>Elija una opción</option>
+                        <option value="nuevo">Nuevo</option>
+                        <option value="reparacion">Reparación</option>
+                    </select>
+                    {errors.tipotrabajo && <span>Este campo es necesario</span>}
+
+                    <label className="text-[20px] font-semibold"> Material </label>
+                    <textarea id="materiales_requeridos"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Materiales requeridos" {...register("materialtrabajo")}></textarea>
+
+                    <label className="text-[20px] font-semibold"> Precio del material </label>
+                    <input id="precio_material" type="number"
+                        className="border-[1px] border-[#3B315F] p-[5px] rounded-[5px] w-[250px] text-[18px]  focus:border-[#3B315F]
+                    focus:outline-none focus:ring-1 focus:ring-[#3B315F]"
+                        placeholder="Precio del material" step="any" {...register("preciomaterial")} />
+                </div>
+
+                <label className="text-[20px] font-semibold"> Seleccione el trabajador </label>
+                {trabajadores.map((trabajador) => (
+                    <div key={trabajador.idtrabajador}>
+                        <input id="trabaador" type="checkbox" value={trabajador.idtrabajador} onChange={() => handleCheckboxChange(trabajador.idtrabajador)} />
+                        <label htmlFor={`trabajador_${trabajador.idtrabajador}`}>
+                            {trabajador.nom_trabajador} {trabajador.apepaterno} {trabajador.apematerno} {trabajador.tipotrabajador}
+                        </label>
+                    </div>
+                ))}
+
+                <button type="submit" className="mt-[20px] bg-[#3B315F] p-[10px] text-white text-[20px] font-sans font-medium rounded hover:bg-[#4D407E]">Registrar</button>
             </form>
         </>
     )
