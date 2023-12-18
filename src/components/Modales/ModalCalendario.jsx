9021509bb7ad364dbcd906de6bf4c81e81d54f3a
Mayreli Romero <may70981@gmail.com>
2023-12-18 12:37:33 -0600

    Estilo del modal

--- src/components/Modales/ModalCalendario.jsx
+++ src/components/Modales/ModalCalendario.jsx
@@ -3,16 +3,20 @@ export function ModalCalendario({ onClose, orden }) {
 
     return (
         <div className="fixed inset-0 flex justify-center items-center z-50">
-            <div className="flex flex-col justify-center items-center bg-cyan-600 p-5 rounded ">
+            <div className="flex flex-col justify-center items-center bg-[#3B315F] p-5 rounded ">
 
                 <div key={orden.idorden}>
-                    <p>Número de orden: {orden.idorden}</p>
-                    <p>Cliente: {orden.nombre_c} {orden.apepaterno_c} {orden.apematerno_c}</p>
-                    <p>Detalles del trabajo: {orden.especificacion}</p>                    
-                   
+                    <p className="text-white text-[18px] mt-4">Número de orden: {orden.idorden}</p>
+                    <p className="text-white text-[18px] mt-4">Cliente: {orden.nombre_c} {orden.apepaterno_c} {orden.apematerno_c}</p>
+                    <p className="text-white text-[18px] mt-4">Detalles del trabajo: {orden.especificacion}</p>
+
                 </div>
 
-                <button onClick={onClose} className="text-white mt-4 px-4 py-2 bg-red-500 rounded">
+                <button onClick={onClose} className="text-[#3B315F] text-[18px] mt-4 px-4 bg-white py-2 rounded">
+                    Ver completo
+                </button>
+
+                <button onClick={onClose} className="text-white text-[18px] mt-4 px-4 py-2 bg-red-500 rounded">
                     Cerrar
                 </button>
             </div>
