9a4d9ee6b74984501693fa371a840e8c586e7e81
Mayreli Romero <may70981@gmail.com>
2023-12-18 17:56:36 -0600

    Separación de Views

---
+++ src/Views/V_VisualizarOrden.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { VisualizarOrden } from '../components/Administrador/VisualizarOrden'
+
+function V_VisualizarOrden() {
+    return (
+      <>
+      <VisualizarOrden/>      
+      </>
+    )
+  }
+  
+  export default V_VisualizarOrden
